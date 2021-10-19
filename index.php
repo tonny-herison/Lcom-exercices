4782380a23cee9f94ee3dfe63a5095b3a501649e
Charla19 <charlap20.aps2a@gmail.com>
2021-10-19 23:53:24 +0300

    Formulaire du customer

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width, initial-scale">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <title>Formulaire</title>
+</head>
+<body>
+    <div class="container">
+        <ul class="form-container"
+            <li>
+                <div><h2>CUSTOMER LOGIN</h2></div>
+            </li>
+            <li>
+                <div class="login-container">
+                    <i class="fas fa-user"></i> <input type="text" name="username" placeholder="Username">
+                    <hr>
+                    <i class="fas fa-lock"></i> <input type="text" name="password" placeholder="********">
+                </div>
+            </li>
+            <li>
+                <div class="button">
+                    <input type="button" value="LOGIN">
+                </div>
+            </li>
+        </ul>
+    </div>
+</body>
