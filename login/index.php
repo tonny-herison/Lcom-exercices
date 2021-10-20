271263f268a5e113e8b3b5d9bad5b717b345b340
Charla19 <charlap20.aps2a@gmail.com>
2021-10-20 10:52:44 +0300

    correction du formulaire

--- login/index.php
+++ login/index.php
@@ -2,28 +2,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width, initial-scale">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <title>Formulaire</title>
 </head>
 <body>
     <div class="container">
-        <ul class="form-container"
-            <li>
+        <form action="" class="form-container">
                 <div><h2>CUSTOMER LOGIN</h2></div>
-            </li>
-            <li>
                 <div class="login-container">
-                    <i class="fas fa-user"></i> <input type="text" name="username" placeholder="Username">
-                    <hr>
-                    <i class="fas fa-lock"></i> <input type="text" name="password" placeholder="********">
+                    <i class="fas fa-user" style="color:rgb(146, 161, 189)"></i> <input type="text" name="username" placeholder="Username">
+                    <hr></hr>
+                    <i class="fas fa-lock" style="color:rgb(146, 161, 189)"></i> <input type="text" name="password" placeholder="********">
                 </div>
-            </li>
-            <li>
                 <div class="button">
-                    <input type="button" value="LOGIN">
+                    <button type="submit">LOGIN</button>
                 </div>
-            </li>
-        </ul>
+        </form>
     </div>
 </body>
