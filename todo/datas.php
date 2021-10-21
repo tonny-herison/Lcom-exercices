ab282abd5a4904ec66b2cd001257098801327a40
Tonny <hartonnyjack@gmail.com>
2021-10-21 18:06:26 +0300

    Todo list

---
+++ todo/datas.php
@@ -0,0 +1,66 @@
+<?php
+
+$work = [
+	[
+		"title" => "Meeting with a client",
+		"description" => "John Doe from TexhX company has to meet me today about products feedback",
+		"from" => date("d-m-Y h:i", strtotime("10:30pm April 15 2014")),
+		"to" => date("d-m-Y h:i", strtotime("12:30pm April 15 2014")),
+		"status" => null,
+	],
+	[
+		"title" => "WebHeroes",
+		"description" => "Prepare new screen",
+		"from" => date("d-m-Y h:i", strtotime("11:30pm April 15 2014")),
+		"to" => date("d-m-Y h:i", strtotime("13:30pm April 15 2014")),
+		"status" => null,
+	],
+	[
+		"title" => "Trustedoctor",
+		"description" => "Make all fixes",
+		"from" => date("d-m-Y h:i", strtotime("11:00pm April 15 2014")),
+		"to" => date("d-m-Y h:i", strtotime("12:00pm April 15 2014")),
+		"status" => true,
+	],
+];
+
+$home = [
+	[
+		"title" => "Grocery Shopping",
+		"description" => "Shopping list : 2 x Roll, apple juice.",
+		"from" => date("d-m-Y h:i", strtotime("10:30pm April 15 2014")),
+		"to" => date("d-m-Y h:i", strtotime("12:30pm April 15 2014")),
+		"status" => null,
+	],
+	[
+		"title" => "Dinner",
+		"description" => "Chicken wings & hot sauce.",
+		"from" => date("d-m-Y h:i", strtotime("11:30pm April 15 2014")),
+		"to" => date("d-m-Y h:i", strtotime("13:30pm April 15 2014")),
+		"status" => null,
+	],
+	[
+		"title" => "Dinner",
+		"description" => null,
+		"from" => date("d-m-Y h:i", strtotime("11:30pm April 15 2014")),
+		"to" => date("d-m-Y h:i", strtotime("13:30pm April 15 2014")),
+		"status" => null,
+	],
+];
+
+$other = [
+	[
+		"title" => "Meeting with friends",
+		"description" => "At Green Beer Pub",
+		"from" => date("d-m-Y h:i", strtotime("10:30pm April 15 2014")),
+		"to" => date("d-m-Y h:i", strtotime("12:30pm April 15 2014")),
+		"status" => null,
+	],
+	[
+		"title" => "Give back camera",
+		"description" => "Return to Michael",
+		"from" => date("d-m-Y h:i", strtotime("11:30pm April 15 2014")),
+		"to" => date("d-m-Y h:i", strtotime("13:30pm April 15 2014")),
+		"status" => false,
+	],
+];
